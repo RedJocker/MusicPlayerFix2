163f6498958151abb57c0a5369b4faad80d1828a
RedJocker <mauricio.guaruja@gmail.com>
2022-04-30 15:36:18 -0300

    stage1 - avoiding holding references to activities and other test improvements

--- Music Player/Stage 1/src/androidTest/java/org/hyperskill/musicplayer/AbstractTest.kt
+++ Music Player/Stage 1/src/androidTest/java/org/hyperskill/musicplayer/AbstractTest.kt
@@ -1,20 +1,90 @@
 package org.hyperskill.musicplayer
 
 import android.app.Activity
-import android.content.Context
+import android.media.MediaPlayer
 import android.view.View
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 
-abstract class AbstractUnitTest<T : Activity>{
+abstract class AbstractTest<T : Activity>{
 
-    protected fun Activity.id(idString: String) : Int {
-        return resources.getIdentifier(idString, "id", packageName)
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    /*
+        Assumes id to be constants based on this
+        https://stackoverflow.com/questions/64951792/are-integer-value-of-view-ids-are-always-constant-in-android
+        so it should not be a problem to hold references to ids
+    */
+    protected fun getViewId(idString: String) : Int {
+        var id = 0;
+        activityRule.scenario.onActivity { activity ->
+            id = activity.id(idString)
+        }
+        return id
     }
 
-    protected fun Context.id(id: String) : Int {
-        return resources.getIdentifier(id, "id", packageName)
+    /**
+     * Use this method to make assertions directly on views.
+     * Prefer using espresso.ViewAssertion if possible (ex: onView(...).check {...})
+     *
+     * Don't hold direct references to views outside this method for the same reason
+     * you should not hold direct references to activity outside of activityRule.scenario.onActivity,
+     * that is, because activity can be recreated at anytime during state transitions.
+     * */
+    protected inline fun <reified V: View> assertView(viewId: Int, crossinline assertion: (V) -> Unit){
+        activityRule.scenario.onActivity {  activity ->
+            val view = findExistingViewById<V>(activity, viewId)
+            assertion(view)
+        }
     }
 
-    protected fun <V: View> Activity.find(idString: String) : V = findViewById(id(idString))
+    /**
+     * Use this method to make assertions on the player.
+     * Don't hold direct references to player outside this method for the same reason
+     * you should not hold direct references to activity outside of activityRule.scenario.onActivity,
+     * that is, because activity can be recreated at anytime during state transitions.
+     * */
+    protected fun assertPlayer(assertion: (MediaPlayer) -> Unit) {
+        activityRule.scenario.onActivity { activity :Activity ->
+            assertion(activity.getMusicPlayer())
+        }
+    }
 
-    protected fun <T: View> View.find(idString: String): T = findViewById(context.id(idString))
+    /**
+     * On classes that implement AbstractTest prefer using assertView instead.
+     *
+     * Don't use this method outside of activityRule.scenario.onActivity.
+     *
+     * Don't keep holding direct references to views for the same reason
+     * you should not hold direct references to activity outside of activityRule.scenario.onActivity,
+     * that is, because activity can be recreated at anytime during state transitions.
+     */
+    /* this method had to be protected instead of private because it had to have the same level
+     as its consumers because of inline, but it should be thought as being private */
+    protected inline fun <reified V: View> findExistingViewById(activity: Activity, id: Int): V {
+       val view: View? = activity.findViewById(id)
+
+       val idNotFoundMessage = "View with id number \"$id\" was not found"
+       val wrongClassMessage = "View with id number \"$id\" is not from expected class. " +
+               "Expected ${V::class.java.simpleName} found ${view?.javaClass?.simpleName}"
+
+       assertNotNull(idNotFoundMessage, view)
+       assertTrue(wrongClassMessage, view is V)
+
+       return view as V
+    }
+
+    private fun Activity.id(idString: String) : Int {
+        val identifier = resources.getIdentifier(idString, "id", packageName)
+        println("idString: $idString, id: $identifier")
+        return identifier
+    }
+
+    private fun Activity.getMusicPlayer(): MediaPlayer {
+        assert(this is MusicPlayable) { "Your Activity should implement the MusicPlayable interface" }
+        return (this as MusicPlayable).player
+    }
 }
--- Music Player/Stage 1/src/androidTest/java/org/hyperskill/musicplayer/Stage1Test.kt
+++ Music Player/Stage 1/src/androidTest/java/org/hyperskill/musicplayer/Stage1Test.kt
@@ -1,90 +1,94 @@
 package org.hyperskill.musicplayer
 
-import android.app.Activity
-import android.media.MediaPlayer
-import androidx.lifecycle.Lifecycle
+
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isClickable
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class Stage1Test: AbstractUnitTest<MainActivity>() {
+class Stage1Test: AbstractTest<MainActivity>() {
 
-    @Rule @JvmField
-    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+    private val playPauseButtonId: Int by lazy { getViewId("play_pause_button") }
+    private val stopButtonId: Int by lazy { getViewId("stop_button") }
+    private val songTitleId: Int by lazy { getViewId("song_title") }
+    private val songArtistId: Int by lazy { getViewId("song_artist") }
 
     @Test
     fun checkIsSongTitleExist() {
-
-        activityRule.scenario.onActivity { activity: Activity ->
-            val message = "Is song title visible for user?"
-            assertNotNull(message, activity.find("song_title"))
-        }
+        assertView<TextView>(songTitleId) {}
     }
 
     @Test
     fun checkIsSongArtistExist() {
-        activityRule.scenario.onActivity { activity: Activity ->
-            val message = "Is song artist name visible for user?"
-            assertNotNull(message, activity.find("song_artist"))
-        }
-
+        assertView<TextView>(songArtistId) {}
     }
 
     @Test
     fun checkIsPlayPauseButtonExist() {
-        activityRule.scenario.onActivity { activity: Activity ->
-            val message = "Is play_pause_button visible for user?"
-            assertNotNull(message, activity.find("play_pause_button"))
-        }
+        assertView<ImageButton>(playPauseButtonId) {}
     }
 
     @Test
     fun checkIsStopButtonExist() {
-        activityRule.scenario.onActivity { activity: Activity ->
-            val message = "Is stop_button visible for user?"
-            assertNotNull(message, activity.find("stop_button"))
-        }
+        assertView<ImageButton>(stopButtonId) {}
     }
 
     @Test
     fun checkIsPlayPauseButtonWorks() {
-        activityRule.scenario.onActivity { activity: Activity ->
-            val message = "Does play_pause_button controls playback?"
-            val player = activity.getMusicPlayer()
-            onView(withId(activity.id("play_pause_button"))).perform(click()).check { view, noViewFoundException ->
-                assertEquals(message, true, player.isPlaying)
-            }.perform(click()).check { view, noViewFoundException ->
-                assertEquals(message, true, player.isPlaying)
+        val message = "Does play_pause_button controls playback?"
+
+        onView(withId(playPauseButtonId))
+            .perform(click())
+            .check { _, _ ->
+                assertPlayer { player ->
+                    assertEquals(message, true, player.isPlaying)
+                }
+            }.perform(click())
+            .check { _, _ ->
+                assertPlayer { player ->
+                    assertEquals(message, false, player.isPlaying)
+                }
+            }.perform(click())
+            .check { _, _ ->
+                assertPlayer { player ->
+                    assertEquals(message, true, player.isPlaying)
+                }
+            }.perform(click())
+            .check { _, _ ->
+                assertPlayer { player ->
+                    assertEquals(message, false, player.isPlaying)
+                }
             }
-        }
     }
 
     @Test
     fun checkIsStopButtonWorks() {
-        activityRule.scenario.onActivity { activity: Activity ->
-            val playPauseButton = activity.id("play_pause_button")
-            val stopButton = activity.id("stop_button")
-            val player = activity.getMusicPlayer()
-            onView(withId(playPauseButton)).perform(click()).check(matches(isClickable()))
-            val message = "Does stop button stops playback?"
-            onView(withId(stopButton)).perform(click())
+        val message = "Does stop button stops playback?"
+
+        onView(withId(playPauseButtonId)).perform(click())
+
+        assertPlayer { player ->
+            assertEquals(message, true, player.isPlaying)
+        }
+
+        onView(withId(stopButtonId)).perform(click())
+
+        assertPlayer { player ->
             assertEquals(message, false, player.isPlaying)
         }
-    }
 
-    private fun Activity.getMusicPlayer(): MediaPlayer {
-        assert(this is MusicPlayable) { "Your Activity should implement the MusicPlayable interface" }
-        return (this as MusicPlayable).player
+        onView(withId(playPauseButtonId)).perform(click())
+
+        val messageResume = "Can you resume playback after clicking stop button"
+
+        assertPlayer { player ->
+            assertEquals(messageResume, true, player.isPlaying)
+        }
     }
 }
