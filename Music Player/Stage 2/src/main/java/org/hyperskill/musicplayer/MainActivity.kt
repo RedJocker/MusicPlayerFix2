f0bd54d20d7ee94c3d933f34f0aff1881cbc075d
RedJocker <mauricio.guaruja@gmail.com>
2022-05-01 02:17:31 -0300

    stage2 - improving solution

--- Music Player/Stage 2/src/main/java/org/hyperskill/musicplayer/MainActivity.kt
+++ Music Player/Stage 2/src/main/java/org/hyperskill/musicplayer/MainActivity.kt
@@ -3,56 +3,86 @@ package org.hyperskill.musicplayer
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.SeekBar
-import android.widget.TextView
+import org.hyperskill.musicplayer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-  lateinit var player: MediaPlayer
+class MainActivity : AppCompatActivity(), MusicPlayable {
+
+  override lateinit var player: MediaPlayer
   lateinit var timer: Timer
-  lateinit var seekBar: SeekBar
-  lateinit var currentTimeView: TextView
-  lateinit var totalTimeView: TextView
+
+  private val binding by lazy {
+    ActivityMainBinding.inflate(layoutInflater)
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(binding.root)
+
     player = MediaPlayer.create(this, R.raw.wisdom)
-    seekBar = findViewById(R.id.seekBar)
-    totalTimeView = findViewById(R.id.total_time_view)
-    currentTimeView = findViewById(R.id.current_time_view)
-    timer = Timer(seekBar, player, currentTimeView)
-    totalTimeView.text = timer.calcTime(player.duration)
-    val playPauseButton: ImageButton = findViewById(R.id.play_pause_button)
-    playPauseButton.setOnClickListener {
-      if (player.isPlaying) {
-        player.pause()
-        timer.stop()
-      } else {
-        player.start()
-        timer.start()
-      }
-    }
-    val stopButton: ImageButton = findViewById(R.id.stop_button)
-    stopButton.setOnClickListener {
-      player.stop()
-      timer.reset()
-    }
-    seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
-      override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-        if(fromUser) {
-          currentTimeView.text = timer.calcTime(progress * 1000)
+    timer = Timer(player, ::onTimerTick , ::onTimerStop)
+
+    binding.totalTimeTextView.text = Timer.timeString(player.duration)
+    binding.seekBar.max = player.duration / 1000
+
+    bindListeners()
+  }
+
+  private fun bindListeners() {
+    binding.apply {
+
+      playPauseButton.setOnClickListener {
+        if (player.isPlaying) {
+          player.pause()
+          timer.pause()
+        } else {
+          player.start()
+          timer.start()
         }
       }
-      override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+      stopButton.setOnClickListener {
+        player.seekTo(0)
+        player.stop()
+        player.prepare()
         timer.stop()
       }
-      override fun onStopTrackingTouch(seekBar: SeekBar?) {
-        player.seekTo(seekBar!!.progress * 1000)
-        timer.start()
+
+      seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+          if(fromUser) {
+            binding.currentTimeTextView.text = Timer.timeString(progress * 1000)
+          }
+        }
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {
+          timer.pause()
+        }
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {
+          player.seekTo(seekBar!!.progress * 1000)
+
+          timer.start()
+
+        }
+      })
+    }
+  }
+
+  private fun onTimerTick() {
+    runOnUiThread {
+      timer.apply {
+        binding.seekBar.progress = timeTotalSeconds()
+        binding.currentTimeTextView.text = timeString()
+        //println("onTimerTick: " + timeString())
       }
-    })
-    seekBar.max = player.duration / 1000
-    timer.runTracker()
+    }
+  }
+
+  private fun onTimerStop() {
+    runOnUiThread {
+      timer.apply {
+        binding.seekBar.progress = 0
+        binding.currentTimeTextView.text = timeString()
+      }
+    }
   }
 }
--- Music Player/Stage 2/src/main/java/org/hyperskill/musicplayer/Timer.kt
+++ Music Player/Stage 2/src/main/java/org/hyperskill/musicplayer/Timer.kt
@@ -3,47 +3,60 @@ package org.hyperskill.musicplayer
 import android.media.MediaPlayer
 import android.os.Handler
 import android.os.Looper
-import android.widget.SeekBar
-import android.widget.TextView
 import java.lang.Runnable
 import java.util.*
 
-class Timer(seekBar: SeekBar, player: MediaPlayer, timeView: TextView) {
-    var running = false
-    val handler = Handler(Looper.getMainLooper())
-    val mSeekBar = seekBar
-    private val mPlayer = player
-    val mTimeView = timeView
-    private val runnable: Runnable = object: Runnable {
-        override fun run() {
-            if (running) {
-                mSeekBar.progress = getTime() / 1000
-                mTimeView.text = calcTime(getTime())
-                println(calcTime(getTime()))
-            }
-            handler.postDelayed(this, 1000)
+class Timer(private val player: MediaPlayer, private val onTick: () -> Unit, private val onStop: () -> Unit) {
+    companion object {
+        fun timeString(timeInMilliseconds: Int): String {
+            return String.format(
+                Locale.getDefault(),
+                "%02d:%02d",
+                timeInMilliseconds / 60_000, timeInMilliseconds % 60_000 / 1000
+            )
         }
     }
-    fun stop() {
+
+    private var running = false
+    private val handler = Handler(Looper.getMainLooper())
+
+    private val runnable: Runnable = object: Runnable {
+        override fun run() {
+            if(player.currentPosition >= player.duration) {
+                pause()
+            }
+            if (running) {
+                onTick()
+                handler.postDelayed(this, 999)
+            }
+        }
+    }
+
+    fun pause() {
         running = false
+        handler.removeCallbacks(runnable)
+    }
+    fun resume() {
+        running = true
     }
     fun start() {
         running = true
+        runTracker()
     }
-    fun reset() {
+    fun stop() {
         running = false
-        mSeekBar.progress = 0
-        mTimeView.text = calcTime(0)
+        onStop()
+        handler.removeCallbacks(runnable)
     }
-    fun calcTime(time: Int): String {
-        return String.format(
-            Locale.getDefault(),
-            "%02d:%02d",
-            time / 60_000, time % 60_000 / 1000
-        )
+
+    fun timeString(): String {
+        return timeString(player.currentPosition)
     }
-    private fun getTime() = mPlayer.currentPosition
-    fun runTracker() {
+
+    fun timeTotalSeconds() = player.currentPosition / 1000
+
+    private fun runTracker() {
+        handler.removeCallbacks(runnable)
         handler.post(runnable)
     }
 }
