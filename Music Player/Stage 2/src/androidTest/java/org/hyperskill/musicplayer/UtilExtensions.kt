2fe55b1ff732060be75865a25af99a93a9a6bcc0
RedJocker <mauricio.guaruja@gmail.com>
2022-05-01 17:57:41 -0300

    stage2 - doc string

--- Music Player/Stage 2/src/androidTest/java/org/hyperskill/musicplayer/UtilExtensions.kt
+++ Music Player/Stage 2/src/androidTest/java/org/hyperskill/musicplayer/UtilExtensions.kt
@@ -1,5 +1,9 @@
 package org.hyperskill.musicplayer
 
+/**
+ * consider this value to be an amount of seconds and returns a sting with the time representation of
+ * that amount in "clock format", but minutes are not bound to 2 digits (ex: "05:43", "00:00" "100:59")
+ */
 fun Int.secondsToTimeString(): String {
     val seconds = "%02d".format(this % 60)
     val minutes = "%02d".format(this / 60)
