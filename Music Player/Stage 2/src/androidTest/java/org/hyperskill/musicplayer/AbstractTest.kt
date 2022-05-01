f100f19c4ab4130b68dbc2e0c464269ac21688a2
RedJocker <mauricio.guaruja@gmail.com>
2022-05-01 02:17:52 -0300

    stage2 - improving tests

--- Music Player/Stage 2/src/androidTest/java/org/hyperskill/musicplayer/AbstractTest.kt
+++ Music Player/Stage 2/src/androidTest/java/org/hyperskill/musicplayer/AbstractTest.kt
@@ -1,19 +1,138 @@
 package org.hyperskill.musicplayer
 
 import android.app.Activity
+import android.media.MediaPlayer
 import android.view.View
+import android.widget.SeekBar
+import androidx.test.espresso.PerformException
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.*
+import androidx.test.espresso.action.ViewActions.actionWithAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.util.HumanReadables
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Matcher
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Rule
 
-abstract class AbstractUnitTest<T : Activity>(private val activityClass: Class<T>) {
 
-    protected lateinit var activity: T
+abstract class AbstractTest<T : Activity>{
 
-    protected fun identifier(id: String) : Int {
-        return activity.resources.getIdentifier(id, "id", activity.packageName)
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    /*
+       Assumes id to be constants based on this
+       https://stackoverflow.com/questions/64951792/are-integer-value-of-view-ids-are-always-constant-in-android
+       so it should not be a problem to hold references to ids
+    */
+    protected fun getViewId(idString: String) : Int {
+        var id = 0;
+        activityRule.scenario.onActivity { activity ->
+            id = activity.id(idString)
+        }
+        return id
     }
 
-    protected fun <T: View> find(id: Int) : T = activity.findViewById(id)
+    /**
+     * Use this method to make assertions directly on views.
+     *
+     * Prefer using espresso.ViewAssertion if possible (ex: onView(...).check {...})
+     *
+     * Don't hold direct references to views outside this method for the same reason
+     * you should not hold direct references to activity outside of activityRule.scenario.onActivity,
+     * that is, because activity can be recreated at anytime during state transitions.
+     * */
+    protected inline fun <reified V: View> assertView(viewId: Int, crossinline assertion: (V) -> Unit){
+        activityRule.scenario.onActivity {  activity ->
+            val view = findExistingViewById<V>(activity, viewId)
+            assertion(view)
+        }
+    }
 
-    protected fun <T: View> find(id: String) : T = activity.findViewById(identifier(id))
+    /**
+     * Use this method to make assertions on the player.
+     *
+     * Don't hold direct references to player outside this method for the same reason
+     * you should not hold direct references to activity outside of activityRule.scenario.onActivity,
+     * that is, because activity can be recreated at anytime during state transitions.
+     * */
+    protected fun assertPlayer(assertion: (MediaPlayer) -> Unit) {
+        activityRule.scenario.onActivity { activity :Activity ->
+            assertion(activity.getMusicPlayer())
+        }
+    }
 
-    protected fun <T: View> View.find(id: String): T = findViewById(identifier(id))
+    /**
+     * Assert the existence of a particular view, and that it is from class V,
+     * then returns the V view.
+     *
+     * On classes that implement AbstractTest prefer using assertView instead.
+     *
+     * Don't use this method outside of activityRule.scenario.onActivity.
+     *
+     * Don't keep holding direct references to views for the same reason
+     * you should not hold direct references to activity outside of activityRule.scenario.onActivity,
+     * that is, because activity can be recreated at anytime during state transitions.
+     */
+    /* this method had to be protected instead of private because it had to have the same level
+     as its consumers, because of inline, but it should be thought as being private */
+    protected inline fun <reified V: View> findExistingViewById(activity: Activity, id: Int): V {
+        val view: View? = activity.findViewById(id)
+
+        val idNotFoundMessage = "View with id number \"$id\" was not found"
+        val wrongClassMessage = "View with id number \"$id\" is not from expected class. " +
+                "Expected ${V::class.java.simpleName} found ${view?.javaClass?.simpleName}"
+
+        assertNotNull(idNotFoundMessage, view)
+        assertTrue(wrongClassMessage, view is V)
+
+        return view as V
+    }
+
+    private fun Activity.id(idString: String) : Int {
+        val identifier = resources.getIdentifier(idString, "id", packageName)
+        println("idString: $idString, id: $identifier")
+        return identifier
+    }
+
+    private fun Activity.getMusicPlayer(): MediaPlayer {
+        assert(this is MusicPlayable) { "Your Activity should implement the MusicPlayable interface" }
+        return (this as MusicPlayable).player
+    }
+
+    /* based on https://stackoverflow.com/a/39827232/13352218 */
+    fun clickSeekBarAction(progress: Int): ViewAction? {
+        return actionWithAssertions(
+            GeneralClickAction(
+                Tap.SINGLE,
+                SeekBarThumbCoordinatesProvider(progress),
+                Press.PINPOINT,
+                0,
+                0
+            )
+        )
+    }
+
+    private class SeekBarThumbCoordinatesProvider(var progress: Int) : CoordinatesProvider {
+        override fun calculateCoordinates(view: View): FloatArray {
+
+            val seekBar = view as SeekBar
+            val width = seekBar.width - seekBar.paddingLeft - seekBar.paddingRight
+            val progress = if (progress == 0) seekBar.progress.toDouble() else progress.toDouble()
+            val xPosition = (seekBar.paddingLeft + width * progress / seekBar.max).toInt()
+            val xy = getVisibleLeftTop(seekBar)
+            return floatArrayOf(xy[0] + xPosition, xy[1] + 10)
+        }
+
+        companion object {
+            private fun getVisibleLeftTop(view: View): FloatArray {
+                val xy = IntArray(2)
+                view.getLocationOnScreen(xy)
+                return floatArrayOf(xy[0].toFloat(), xy[1].toFloat())
+            }
+        }
+    }
 }
--- Music Player/Stage 2/src/androidTest/java/org/hyperskill/musicplayer/Stage2Test.kt
+++ Music Player/Stage 2/src/androidTest/java/org/hyperskill/musicplayer/Stage2Test.kt
@@ -1,52 +1,213 @@
 package org.hyperskill.musicplayer
 
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Rule
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class Stage2Test: AbstractUnitTest<MainActivity>(MainActivity::class.java) {
+class Stage2Test: AbstractTest<MainActivity>() {
 
-    @Rule @JvmField
-    var activityRule = ActivityTestRule(MainActivity::class.java)
+    private val playPauseButtonId: Int by lazy { getViewId("playPauseButton") }
+    private val stopButtonId: Int by lazy { getViewId("stopButton") }
+    private val songTitleId: Int by lazy { getViewId("songTitleTextView") }
+    private val songArtistId: Int by lazy { getViewId("songArtistTextView") }
+    private val totalTimeViewId: Int by lazy { getViewId("totalTimeTextView") }
+    private val currentTimeViewId: Int by lazy { getViewId("currentTimeTextView") }
+    private val seekBarId: Int by lazy { getViewId("seekBar") }
 
-    @Before
-    fun before() {
-        activity = activityRule.activity
-        println("before")
-    }
 
     @Test
     fun checkIsTotalTimeViewExist() {
-        val message = "Is total_time_view visible for user?"
-        assertNotNull(message, find(R.id.total_time_view))
+        assertView<TextView>(totalTimeViewId) { R.id.currentTimeTextView}
     }
 
     @Test
     fun checkIsCurrentTimeViewExist() {
-        val message = "Is current_time_view visible for user?"
-        assertNotNull(message, find(R.id.current_time_view))
+        assertView<TextView>(currentTimeViewId) {}
     }
 
     @Test
     fun checkIsSeekbarExist() {
-        val message = "Is seekbar visible for user?"
-        assertNotNull(message, find(R.id.seekBar))
+        assertView<SeekBar>(seekBarId) {}
     }
 
     @Test
-    fun checkIsSeekbarMoves() {
-        val message = "Is seekbar progress changes while music playing?"
-        onView(withId(R.id.play_pause_button)).perform(click())
-        Thread.sleep(2000)
-        assertEquals(message, true, activity.seekBar.progress > 0)
+    fun checkSeekbarInitialValues() {
+        val messageStart = "seekBar should start with progress at the beginning"
+        val messageMax = "seekBar should have max value matching the song duration in seconds"
+        val expectedInitialProgress = 0
+        val expectedInitialMax = 214
+        val seekBarInteraction: ViewInteraction = onView(withId(seekBarId))
+
+        seekBarInteraction.check { view, _ ->
+            val seekBar = (view as SeekBar)
+
+            val actualInitialProgress = seekBar.progress
+            assertEquals(messageStart, expectedInitialProgress, actualInitialProgress)
+
+            val actualInitialMax = seekBar.max
+            assertEquals(messageMax, expectedInitialMax, actualInitialMax)
+        }
+    }
+
+    @Test
+    fun checkTimeTextViewsInitialValues() {
+
+        val messageInitialCurrentTimeView = "currentTimeTextView should display the initial value"
+        val messageInitialTotalTimeView = "totalTimeTextView should display the music total duration"
+
+        val currentTimeViewInteraction: ViewInteraction = onView(withId(currentTimeViewId))
+        val totalTimeViewInteraction: ViewInteraction = onView(withId(totalTimeViewId))
+
+        currentTimeViewInteraction.check { view, _ ->
+            val currentTimeTextView = view as TextView
+            val expectedInitialTime = "00:00"
+            val actualInitialTime = currentTimeTextView.text.toString()
+            assertEquals(messageInitialCurrentTimeView, expectedInitialTime, actualInitialTime)
+        }
+
+        totalTimeViewInteraction.check { view, _ ->
+            val totalTimeTextView = view as TextView
+            val expectedInitialTime = "03:34"
+            val actualInitialTime = totalTimeTextView.text.toString()
+            assertEquals(messageInitialTotalTimeView, expectedInitialTime, actualInitialTime)
+        }
+    }
+
+    @Test
+    fun checkIsSeekbarMoving() {
+        val messageStart = "seekBar should start with progress at the beginning"
+        val messageProgressChanges = "Is seekbar progress changing while music playing?"
+        val messageMatchProgress = "The progress of seekBar should match `player.currentPosition / 1000`"
+
+        val seekBarInteraction: ViewInteraction = onView(withId(seekBarId))
+        val playPauseButtonInteraction: ViewInteraction = onView(withId(playPauseButtonId))
+        val stopInteraction: ViewInteraction = onView(withId(stopButtonId))
+
+        seekBarInteraction.check { view, _ ->
+            val expectedProgress = 0
+            val actualProgress: Int = (view as SeekBar).progress
+            assertEquals(messageStart, expectedProgress, actualProgress)
+        }
+
+        playPauseButtonInteraction.perform(click())
+        Thread.sleep(1800)
+
+        seekBarInteraction.check { view, _ ->
+            val actualProgress: Int = (view as SeekBar).progress
+            assertTrue(messageProgressChanges, actualProgress > 0)
+
+            assertPlayer { player ->
+                val playerDuration = player.currentPosition / 1000
+                val actualProgress2 = view.progress
+                assertEquals(messageMatchProgress, playerDuration, actualProgress2)
+            }
+        }
+        stopInteraction.perform(click())
+    }
+
+    @Test
+    fun checkStateAfterSeekBarTrackingTouch() {
+        val seekBarInteraction: ViewInteraction = onView(withId(seekBarId))
+        val currentTimeInteraction = onView(withId(currentTimeViewId))
+        val playPauseButtonInteraction: ViewInteraction = onView(withId(playPauseButtonId))
+        val totalTimeViewInteraction: ViewInteraction = onView(withId(totalTimeViewId))
+        val stopInteraction: ViewInteraction = onView(withId(stopButtonId))
+        val progressSetByTest = 71
+
+        seekBarInteraction.perform(clickSeekBarAction(progressSetByTest))
+
+        val messageCurrentTextViewChangeExpected =
+            "Changing the seekBar progress should change the currentTimeTextView"
+        currentTimeInteraction.check { view, _ ->
+            val actualCurrentTime = (view as TextView).text
+            val expectedCurrentTime = progressSetByTest.secondsToTimeString()
+            assertEquals(messageCurrentTextViewChangeExpected, expectedCurrentTime, actualCurrentTime)
+        }
+
+        val messagePlayerChangeExpected =
+            "Changing the seekBar progress should change the player current position"
+        assertPlayer { player ->
+            val actualCurrentPosition = player.currentPosition
+            val expectedCurrentPosition = progressSetByTest * 1000
+            assertEquals(messagePlayerChangeExpected, expectedCurrentPosition, actualCurrentPosition)
+        }
+
+        val messageTotalTimeShouldNotChange =
+            "totalTimeTextView should not change text value after seekBar changes"
+        totalTimeViewInteraction.check { view, _ ->
+            val totalTimeTextView = view as TextView
+            val expectedTime = "03:34"
+            val actualTime = totalTimeTextView.text.toString()
+            assertEquals(messageTotalTimeShouldNotChange, expectedTime, actualTime)
+        }
+
+        playPauseButtonInteraction.perform(click())
+        Thread.sleep(1800)
+
+        val messageProgressChanges =
+            "After changing the seekBar progress and clicking on playButton, progress should resume based on new value"
+        val messageMatchProgress =
+            "After changing the seekBar progress and clicking on playButton, player should start based on new value"
+
+        seekBarInteraction.check { view, _ ->
+            val actualProgress: Int = (view as SeekBar).progress
+            assertTrue(messageProgressChanges, actualProgress > progressSetByTest)
+
+            assertPlayer { player ->
+                val playerDuration = player.currentPosition / 1000
+                val actualProgress2 = view.progress
+                assertTrue(messageMatchProgress, playerDuration > progressSetByTest)
+                assertEquals(messageMatchProgress, playerDuration, actualProgress2)
+            }
+        }
+
+
+        stopInteraction.perform(click())
+        val messageCurrentTextViewChangeAfterStop =
+            "After clicking on stopButton currentTimeTextView should change back to initial value"
+        currentTimeInteraction.check { view, _ ->
+            val actualCurrentTime = (view as TextView).text
+            val expectedCurrentTime = "00:00"
+            assertEquals(messageCurrentTextViewChangeAfterStop, expectedCurrentTime, actualCurrentTime)
+        }
+    }
+
+    @Test
+    fun checkSeekBarAfterStop() {
+        val seekBarInteraction: ViewInteraction = onView(withId(seekBarId))
+        val currentTimeInteraction = onView(withId(currentTimeViewId))
+        val playPauseButtonInteraction: ViewInteraction = onView(withId(playPauseButtonId))
+        val stopInteraction: ViewInteraction = onView(withId(stopButtonId))
+        val progressSetByTest = 122
+
+        playPauseButtonInteraction.perform(click())
+        stopInteraction.perform(click())
+
+        seekBarInteraction.perform(clickSeekBarAction(progressSetByTest))
+
+        val messageCurrentTextViewChangeExpected =
+            "After stopButton clicked changing the seekBar progress should change the currentTimeTextView"
+        currentTimeInteraction.check { view, _ ->
+            val actualCurrentTime = (view as TextView).text
+            val expectedCurrentTime = progressSetByTest.secondsToTimeString()
+            assertEquals(messageCurrentTextViewChangeExpected, expectedCurrentTime, actualCurrentTime)
+        }
+
+        val messagePlayerChangeExpected =
+            "After stopButton clicked changing the seekBar progress should change the player current position"
+        assertPlayer { player ->
+            val actualCurrentPosition = player.currentPosition
+            val expectedCurrentPosition = progressSetByTest * 1000
+            assertEquals(messagePlayerChangeExpected, expectedCurrentPosition, actualCurrentPosition)
+        }
     }
 }
