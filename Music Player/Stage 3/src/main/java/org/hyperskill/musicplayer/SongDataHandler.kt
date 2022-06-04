2c087482238286e169a9cfce04a7b55e4cd50cec
RedJocker <mauricio.guaruja@gmail.com>
2022-06-04 02:22:25 -0300

    stage3 - refactoring getSongList

--- Music Player/Stage 3/src/main/java/org/hyperskill/musicplayer/SongDataHandler.kt
+++ Music Player/Stage 3/src/main/java/org/hyperskill/musicplayer/SongDataHandler.kt
@@ -4,29 +4,28 @@ import android.content.ContentResolver
 import android.provider.MediaStore
 
 fun getSongList(musicResolver: ContentResolver): List<Song> {
-    val songList = mutableListOf<Song>()
-
     val musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+    val projection = arrayOf(
+        MediaStore.Audio.Media._ID,
+        MediaStore.Audio.Media.ARTIST,
+        MediaStore.Audio.Media.TITLE,
+        MediaStore.Audio.Media.DURATION
+    )
 
-    val musicCursor = musicResolver.query(musicUri, null, null,
+    val musicCursor = musicResolver.query(musicUri, projection, null,
         null, null, null)
 
-    musicCursor?.use {
-        if (it.moveToFirst()) {
-            val titleColumn = it.getColumnIndex(MediaStore.Audio.Media.TITLE)
-            val artistColumn = it.getColumnIndex(MediaStore.Audio.Media.ARTIST)
-            val idColumn = it.getColumnIndex(MediaStore.Audio.Media._ID)
-            val durationColumn = it.getColumnIndex(MediaStore.Audio.Media.DURATION)
-
-            do {
-                val title = it.getString(titleColumn)
-                val artist = it.getString(artistColumn)
-                val id = it.getLong(idColumn)
-                val duration  = it.getLong(durationColumn)
-                songList.add(Song(title, artist, id, duration))
-            } while (it.moveToNext())
-        }
-    }
-
-    return songList
+    return musicCursor?.use {
+        generateSequence {
+            if(it.moveToNext()) {
+                val id = it.getLong(0)
+                val artist = it.getString(1)
+                val title = it.getString(2)
+                val duration  = it.getLong(3)
+                Song(title, artist, id, duration)
+            } else {
+                null
+            }
+        }.toList()
+    } ?: listOf()
 }
