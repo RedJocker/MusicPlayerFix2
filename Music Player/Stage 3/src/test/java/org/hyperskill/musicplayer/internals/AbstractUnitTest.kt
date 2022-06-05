8de2fc5e05a9f7901d48d08f5b3b38180655a578
RedJocker <mauricio.guaruja@gmail.com>
2022-06-05 14:59:45 -0300

    stage3 - improving documentation and changing visibility to public on AbstractUnitTest.kt

--- Music Player/Stage 3/src/test/java/org/hyperskill/musicplayer/internals/AbstractUnitTest.kt
+++ Music Player/Stage 3/src/test/java/org/hyperskill/musicplayer/internals/AbstractUnitTest.kt
@@ -11,6 +11,7 @@ import org.robolectric.Robolectric
 import org.robolectric.Shadows.shadowOf
 import org.robolectric.android.controller.ActivityController
 import org.robolectric.annotation.Config
+import org.robolectric.shadow.api.Shadow
 import org.robolectric.shadows.ShadowActivity
 import org.robolectric.shadows.ShadowLooper
 import java.time.Duration
@@ -18,30 +19,50 @@ import java.time.Duration
 @Config(shadows = [CustomMediaPlayerShadow::class])
 abstract class AbstractUnitTest<T : Activity>(clazz: Class<T>) {
 
-    private val activityController: ActivityController<T> by lazy {
+    /**
+     * Setup and control activities and their lifecycle
+     */
+    val activityController: ActivityController<T> by lazy {
         Robolectric.buildActivity(clazz)
     }
 
-    protected val activity : Activity by lazy {
-
+    /**
+     * The activity being tested.
+     *
+     * It is the @RealObject of the shadowActivity
+     */
+    val activity : Activity by lazy {
         activityController.get()
     }
 
-    protected val shadowActivity: ShadowActivity by lazy {
-        shadowOf(activity)
+    /**
+     * A Roboletric shadow object of the Activity class, contains helper methods to deal with
+     * testing activities like setting permissions, peeking results of launched activities for result,
+     * retrieving shown dialogs, intents and others.
+     *
+     * If you don't know what shadows are you can have a better understanding on that reading this
+     * on roboletric documentation: http://robolectric.org/extending/
+     */
+    val shadowActivity: ShadowActivity by lazy {
+        Shadow.extract(activity)
     }
 
-    protected val shadowLooper: ShadowLooper by lazy {
+    /**
+     * A Roboletric shadow object of the mainLooper. Handles enqueued runnables and also the passage of time.
+     *
+     * Usually used with .idleFor(someDurationValue) or .runToEndOfTasks()
+     */
+    val shadowLooper: ShadowLooper by lazy {
         shadowOf(activity.mainLooper)
     }
-
-
 
     /**
      * Decorate your test code with this method to ensure better error messages displayed
      * when tests are run with check button and exceptions are thrown by user implementation.
+     *
+     * returns a value for convenience use, like in tests that involve navigation between Activities
      */
-    fun testActivity(arguments: Intent = Intent(), testCodeBlock: (Activity) -> Unit) {
+    fun <ReturnValue> testActivity(arguments: Intent = Intent(), testCodeBlock: (Activity) -> ReturnValue): ReturnValue {
         try {
             activity.intent =  arguments
             activityController.setup()
@@ -49,7 +70,7 @@ abstract class AbstractUnitTest<T : Activity>(clazz: Class<T>) {
             throw AssertionError("Exception, test failed on activity creation with $ex\n${ex.stackTraceToString()}")
         }
 
-        try {
+        return try {
             testCodeBlock(activity)
         } catch (ex: Exception) {
             throw AssertionError("Exception. Test failed on activity execution with $ex\n${ex.stackTraceToString()}")
@@ -101,7 +122,7 @@ abstract class AbstractUnitTest<T : Activity>(clazz: Class<T>) {
      *
      * Internally it calls performClick() and shadowLooper.idleFor(millis)
      */
-    protected fun View.clickAndRun(millis: Long = 500){
+    fun View.clickAndRun(millis: Long = 500){
         this.performClick()
         shadowLooper.idleFor(Duration.ofMillis(millis))
     }
@@ -113,7 +134,7 @@ abstract class AbstractUnitTest<T : Activity>(clazz: Class<T>) {
      *
      * First onStartTrackingTouch(), then onProgressChanged() as user, and finally onStopTrackingTouch()
      */
-    protected fun SeekBar.setProgressAsUser(progress: Int) {
+    fun SeekBar.setProgressAsUser(progress: Int) {
         val shadowSeekBar = shadowOf(this)
         shadowSeekBar.onSeekBarChangeListener.onStartTrackingTouch(this)
 
