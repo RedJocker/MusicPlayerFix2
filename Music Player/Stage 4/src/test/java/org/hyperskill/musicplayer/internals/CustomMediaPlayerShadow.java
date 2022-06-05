86e8aa23734889711ba6a9e2d7c02daded9e44bb
RedJocker <mauricio.guaruja@gmail.com>
2022-06-05 19:55:40 -0300

    stage4 - added @implementation create(Context context, Uri trackUri)

--- Music Player/Stage 4/src/test/java/org/hyperskill/musicplayer/internals/CustomMediaPlayerShadow.java
+++ Music Player/Stage 4/src/test/java/org/hyperskill/musicplayer/internals/CustomMediaPlayerShadow.java
@@ -2,8 +2,12 @@ package org.hyperskill.musicplayer.internals;
 
 import static org.robolectric.shadows.ShadowMediaPlayer.State.INITIALIZED;
 
+import android.content.ContentUris;
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
+
 
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
@@ -11,29 +15,63 @@ import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowMediaPlayer;
 import org.robolectric.shadows.util.DataSource;
 
+import static org.junit.Assert.assertEquals;
+
 @Implements(MediaPlayer.class)
 public class CustomMediaPlayerShadow extends ShadowMediaPlayer {
 
-    private static int fakeSongDuration = 210_000;
+    private static SongFake fakeSong = new SongFake(
+            "-1",
+            "Guggenheim grotto",
+            "Wisdom",
+            210_000
+    );
 
     @Implementation
     public static MediaPlayer create(Context context, int resid){
-        DataSource ds = DataSource.toDataSource(String.valueOf(resid));
-        addMediaInfo(ds, new ShadowMediaPlayer.MediaInfo(fakeSongDuration, 0));
+        DataSource dataSource = DataSource.toDataSource(String.valueOf(resid));
+        MediaInfo info = new MediaInfo(fakeSong.getDuration(), 0);
 
-        MediaPlayer mp = new MediaPlayer();
-        ShadowMediaPlayer shadow = Shadow.extract(mp);
+        addMediaInfo(dataSource, info);
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        ShadowMediaPlayer shadow = Shadow.extract(mediaPlayer);
         try {
-            shadow.setDataSource(ds);
+            shadow.setDataSource(dataSource);
             shadow.setState(INITIALIZED);
-            mp.prepare();
+            mediaPlayer.prepare();
         } catch (Exception e) {
             return null;
         }
-        return mp;
+        return mediaPlayer;
     }
 
-    public static void setFakeSongDuration(int fakeSongDuration) {
-        CustomMediaPlayerShadow.fakeSongDuration = fakeSongDuration;
+    @Implementation
+    public static MediaPlayer create(Context context, Uri trackUri){
+        Uri expectedSongUri = ContentUris.withAppendedId(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                Long.parseLong(fakeSong.getId())
+        );
+
+        assertEquals("Media player created with incorrect uri", expectedSongUri.getPath(), trackUri.getPath());
+
+        DataSource dataSource = DataSource.toDataSource(context, trackUri);
+        MediaInfo info = new MediaInfo((int) fakeSong.getDuration(), 0);
+        addMediaInfo(dataSource, info);
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        ShadowMediaPlayer shadow = Shadow.extract(mediaPlayer);
+        try {
+            shadow.setDataSource(dataSource);
+            shadow.setState(INITIALIZED);
+            mediaPlayer.prepare();
+        } catch (Exception e) {
+            return null;
+        }
+        return mediaPlayer;
+    }
+
+    public static void setFakeSong(SongFake fakeSong) {
+        CustomMediaPlayerShadow.fakeSong = fakeSong;
     }
 }
